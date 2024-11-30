9ef06f44b6da9d203e9a3c070867ce895f38d7fc
Shivam <shivamk.json@gmail.com>
2024-11-30 00:09:42 +0000

    finished neural-networks

---
+++ 05_neural-networks/main.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+mnist = tf.keras.datasets.mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+x_train = tf.keras.utils.normalize(x_train, axis=1)
+x_test = tf.keras.utils.normalize(x_test, axis=1)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(28, 28)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train, epochs=3)
+
+val_loss, val_acc = model.evaluate(x_test, y_test)
+print(f"Validation Loss: {val_loss}, Validation Accuracy: {val_acc}")
